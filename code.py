fcc220ba0d0408b78486f5cb96d962edd0cf0890
dbergmanns <32308080+dbergmanns@users.noreply.github.com>
2017-11-27 19:54:57 -0600

    Update code.py

--- code.py
+++ code.py
@@ -42,7 +42,8 @@ ipums_1516_final.COUNTYFIPS = ipums_1516_final.COUNTYFIPS.astype(str) #convert C
 ipums_1516_final.STATEFIP = ipums_1516_final.STATEFIP.astype(str) 
 ipums_1516_final.dtypes
 ipums_1516_final['COUNTYFIPS'] = ipums_1516_final['COUNTYFIPS'].apply(lambda x: x.zfill(3)) #add leading zeros to COUNTYFIPS
-ipums_1516_filter["STATCOUN"] = ipums_1516_filter["STATEFIP"] + ipums_1516_filter["COUNTYFIPS"] #combine STATEFIP and COUNTYFIPS in one colummn
+ipums_1516_filter["ID"] = ipums_1516_filter["STATEFIP"] + ipums_1516_filter["COUNTYFIPS"] #combine STATEFIP and COUNTYFIPS in one colummn
+#no sé si tengamos que agregar leading zeros a STATEFIP. Checar cómo esta el código en archivos de ACS
 
 #guardar cada archivo
 ipums_1516_final.to_csv('IPUMS_15-16.csv')
